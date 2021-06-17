f9297e164e8bf57729d59726a680bddce40d2fe1
Luke Camelo <llukecamelo@gmail.com>
2021-06-16 20:06:23 -0400

    All functionality implemented

--- src/Components/PokeTile.js
+++ src/Components/PokeTile.js
@@ -1,9 +1,12 @@
-function PokeTile({name, num, img, type}) {
+function PokeTile({name, num, img, type, weaknesses}) {
   return(
     <>
       <div>
         <h1>{name}</h1>
-        {type.map(type => <p>{type}</p>)}
+        <p><strong>type</strong></p>
+        {type.map((type, key) => <p key={key}>{type}</p>)}
+        <p><strong>weaknesses</strong></p>
+        {weaknesses.map((weakness, key) => <p key={key}>{weakness}</p>)}
         <p>#{num}</p>
         <img src={img} style={{width: '100px', height: '100px'}} alt={name}/>
       </div>
--- src/Containers/List.js
+++ src/Containers/List.js
@@ -10,10 +10,17 @@ function List() {
   const [filteredPokemon, setFilteredPokemon] = useState([])
   const [inputValue, setInputValue] = useState("")
   
-  const [checkedState, setCheckedState] = useState(
+  const [checkedStateType, setCheckedStateType] = useState(
+    new Array(types.length).fill(false)
+  )
+
+  const [checkedStateWeakness, setCheckedStateWeakness] = useState(
     new Array(types.length).fill(false)
   )
   
+  const [filteredByType, setFilteredByType] = useState([])
+  const [filteredByWeaknesses, setFilteredByWeaknesses] = useState([])
+
   useEffect(() => {
     const fetchData = async () => {
       const result = await axios(
@@ -21,13 +28,33 @@ function List() {
       )
       
       setPokemonArray(result.data.pokemon)
+      // filtered pokemon includes all 151 by default
       setFilteredPokemon(result.data.pokemon)
     }
 
     fetchData()
   }, [])
 
-  function handleChange(position) {
+
+  useEffect(() => {
+    let filteredPokemon = []
+
+    if (checkedStateType.includes(true) && checkedStateWeakness.includes(true)) {
+      filteredPokemon = filteredByType.filter(value => filteredByWeaknesses.includes(value))
+    } else if (checkedStateType.includes(true)) {
+      filteredPokemon = filteredByType
+    } else if (checkedStateWeakness.includes(true)) {
+      filteredPokemon = filteredByWeaknesses
+    } else {
+      filteredPokemon = pokemonArray
+    }
+
+    setFilteredPokemon(filteredPokemon)
+  }, [checkedStateType, checkedStateWeakness])
+  
+  // Seeing that the only difference between the type and weakness handleChange functions is pokemon.type/pokemon.weaknesses, Ideally I would consolidate them into one function
+  // Which I would have done, had I had the time
+  function handleChangeType(position, checkedState, setCheckedState) {
     const updatedCheckedState = checkedState.map((item, index) => index === position ? !item : item)
 
     setCheckedState(updatedCheckedState)
@@ -38,14 +65,12 @@ function List() {
       }
     })
 
-    let filteredPokemon = pokemonArray.filter(pokemon => {
+    let filtered = pokemonArray.filter(pokemon => {
       if (!updatedCheckedState.includes(true)) {
         return pokemon
-      }
-      else if(pokemon.type.some(type => selectedTypes.includes(type)) && selectedTypes.length < 2) {
+      } else if(pokemon.type.some(type => selectedTypes.includes(type)) && selectedTypes.length < 2) {
         return pokemon
       } else if (selectedTypes.length >= 2) {
-
         // I am not happy with this solution, and in a professional environment I would work to refactor this
         let matches = []
         selectedTypes.forEach(type => {
@@ -62,20 +87,58 @@ function List() {
       }
     })
 
-    setFilteredPokemon(filteredPokemon)
+    setFilteredByType(filtered)
+  }
+
+  function handleChangeWeakness(position, checkedState, setCheckedState) {
+    const updatedCheckedState = checkedState.map((item, index) => index === position ? !item : item)
+
+    setCheckedState(updatedCheckedState)
+    
+    let selectedTypes = types.filter((type, index) => {
+      if(updatedCheckedState[index]) {
+        return type
+      }
+    })
+
+    let filtered = pokemonArray.filter(pokemon => {
+      if (!updatedCheckedState.includes(true)) {
+        return pokemon
+      } else if(pokemon.weaknesses.some(type => selectedTypes.includes(type)) && selectedTypes.length < 2) {
+        return pokemon
+      } else if (selectedTypes.length >= 2) {
+
+        // I am not happy with this solution, and in a professional environment I would work to refactor this
+        let matches = []
+        selectedTypes.forEach(type => {
+          if (pokemon.weaknesses.includes(type)) {
+            matches.push(true)
+          } else {
+            matches.push(false)
+          }
+        })
+
+        if(!matches.includes(false)) {
+          return pokemon
+        }
+      }
+    })
+
+    setFilteredByWeaknesses(filtered)
   }
 
   let pokemonToDisplay = filteredPokemon.filter(pokemon => {
     if (inputValue == null || pokemon.name.toLowerCase().includes(inputValue.toLowerCase())) {
       return pokemon
     }
-  }).map((pokemon, key) => <PokeTile name={pokemon.name} id={pokemon.id} num={pokemon.num} img={pokemon.img} type={pokemon.type} key={key}/>)
+  }).map((pokemon, key) => <PokeTile name={pokemon.name} id={pokemon.id} num={pokemon.num} img={pokemon.img} type={pokemon.type} weaknesses={pokemon.weaknesses} key={key}/>)
 
   return(
     <>
       <input type="text" onChange={e => setInputValue(e.target.value)} />
 
       <ul>
+        <h3>Type</h3>
         {types.map((type, index) => {
           return (
             <li key={index}>
@@ -84,14 +147,34 @@ function List() {
                 id={`type-checkbox-${index}`}
                 name={type} 
                 value={type} 
-                checked={checkedState[index]} 
-                onChange={() => handleChange(index)}
+                checked={checkedStateType[index]} 
+                onChange={() => handleChangeType(index, checkedStateType, setCheckedStateType)}
               />
               <label htmlFor={`type-checkbox-${index}`}>{type}</label>
             </li>
           )
         })}
       </ul>
+
+      <ul>
+        <h3>Weaknesses</h3>
+        {types.map((type, index) => {
+          return (
+            <li key={index}>
+              <input 
+                type="checkbox"
+                id={`weakness-checkbox-${index}`}
+                name={type} 
+                value={type} 
+                checked={checkedStateWeakness[index]} 
+                onChange={() => handleChangeWeakness(index, checkedStateWeakness, setCheckedStateWeakness)}
+              />
+              <label htmlFor={`weakness-checkbox-${index}`}>{type}</label>
+            </li>
+          )
+        })}
+      </ul>
+
       <ul>
         {pokemonToDisplay}
       </ul>
